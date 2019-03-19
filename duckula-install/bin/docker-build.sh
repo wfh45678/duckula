0f8c95f1ea2b10aa8f6473e201c2420797fc4515
andy.zhou <rjzjh@163.com>
2019-03-19 14:23:00 +0800

    修改配置

--- duckula-install/bin/docker-build.sh
+++ duckula-install/bin/docker-build.sh
@@ -13,12 +13,12 @@
 workdir=$1
 version=$2
 #images的分组,hub用"rjzjh"
-group="rjzjh"
+group="192.168.137.100:5000/rjzjh"
 #登陆地址，hub为空
 loginurl="https://hub.docker.com/"
 ## hub.docker.com 上的用户名、密码
 username_push="rjzjh"
-pwd_push="mfkwcwiqpl"
+pwd_push=""
 echo "------docker build------------"
 docker build -f ${workdir}/Dockerfile-data  -t="${group}/duckula:data.${version}" ${workdir}
 docker build -f ${workdir}/Dockerfile-ops   -t="${group}/duckula:ops.${version}"  ${workdir}
@@ -26,7 +26,7 @@ docker build -f ${workdir}/Dockerfile-task  -t="${group}/duckula:task.${version}
 
 if [ $# -ge 3 ]; then
 	echo "------login ------------"
-	docker login ${loginurl}  -u ${username_push} -p ${pwd_push}
+	#docker login ${loginurl}  -u ${username_push} -p ${pwd_push}
 	echo "------push ------------"
 	docker push ${group}/duckula:data.${version}
 	docker push ${group}/duckula:ops.${version}
