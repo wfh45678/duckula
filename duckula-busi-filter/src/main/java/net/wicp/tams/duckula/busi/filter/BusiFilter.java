c691a1fb25637de5e7177fcf404cd9bb18cb4159
andy.zhou <rjzjh@163.com>
2019-12-11 19:24:52 +0800

    过滤

--- duckula-busi-filter/src/main/java/net/wicp/tams/duckula/busi/filter/BusiFilter.java
+++ duckula-busi-filter/src/main/java/net/wicp/tams/duckula/busi/filter/BusiFilter.java
@@ -107,45 +107,47 @@ public class BusiFilter implements IBusi {
 				// 列过滤在后面做，且只会存在一个
 				continue;
 			}
-			Map<String, Map<String, Set<String>>> dbtbMap = filterRulesMap.get(filterPattern);
+			Map<String, Map<String, Set<String>>> dbtbMap = filterRulesMap.get(filterPattern);// 模式sql/col等
 			if (MapUtils.isNotEmpty(dbtbMap) && MapUtils.isNotEmpty(dbtbMap.get(db_tb))) {// 需要过滤
 				String[][] valuestrue = OptType.delete == duckulaPackage.getEventTable().getOptType()
 						? duckulaPackage.getBefores()
 						: duckulaPackage.getAfters();
-				Map<String, Set<String>> fieldmap = dbtbMap.get(db_tb);
+				Map<String, Set<String>> fieldmap = dbtbMap.get(db_tb);// 列名->过滤条件
 				for (String col : fieldmap.keySet()) {
 					// remove.clear();如果有多个过滤条件，是叠加，不要clear
 					int indexOf = "_".equals(col) ? -2
 							: ArrayUtils.indexOf(duckulaPackage.getEventTable().getCols(), col);//
-					for (String field : fieldmap.keySet()) {
-						final CountDownLatch latch = new CountDownLatch(valuestrue.length);
-						String[] values = fieldmap.get(field).toArray(new String[fieldmap.get(field).size()]);
-						for (int i = 0; i < values.length; i++) {
-							final int index = i;
-							ThreadPool.getDefaultPool().submit(new Runnable() {
+					for (String field : fieldmap.keySet()) {// 过滤条件的值
+						String[] values = fieldmap.get(field).toArray(new String[fieldmap.get(field).size()]);// 过滤条件
+						for (int i = 0; i < values.length; i++) {// 每个过滤条件都需要执行一次
+							final int ruleIndex = i;
+							final CountDownLatch latch = new CountDownLatch(valuestrue.length);
+							for (int j = 0; j < valuestrue.length; j++) {// 每个数据都要过滤
+								final int index = j;
+								ThreadPool.getDefaultPool().submit(new Runnable() {
 
-								@Override
-								public void run() {
-									try {
-										filter(duckulaPackage, remove, valuestrue, indexOf, filterPattern,
-												values[index], index);
-									} catch (Exception e) {
-										log.error("过滤失败:" + duckulaPackage.getEventTable().getTb() + ":"
-												+ valuestrue[index][0], e);
-									} finally {
-										latch.countDown();
+									@Override
+									public void run() {
+										try {
+											filter(duckulaPackage, remove, valuestrue, indexOf, filterPattern,
+													values[ruleIndex], index);
+										} catch (Exception e) {
+											log.error("过滤失败:" + duckulaPackage.getEventTable().getTb() + ":"
+													+ valuestrue[index][0], e);
+										} finally {
+											latch.countDown();
+										}
 									}
-								}
-							});
-						}
-						try {
-							latch.await(240, TimeUnit.SECONDS); // latch.await();
-							// log.info("remove:" + remove.size());
-						} catch (InterruptedException e) {
-							log.error("等待CountDownLatch超时", e);
+								});
+							}
+							try {
+								latch.await(240, TimeUnit.SECONDS); // latch.await();
+								// log.info("remove:" + remove.size());
+							} catch (InterruptedException e) {
+								log.error("等待CountDownLatch超时", e);
+							}
 						}
 					}
-
 				}
 			}
 		}
@@ -228,19 +230,29 @@ public class BusiFilter implements IBusi {
 		}
 	}
 
-	private void filter(DuckulaPackage duckulaPackage, Map<Integer, Boolean> remove, String[][] valuestrue, int indexOf,
-			FilterPattern pattern, String value, int i) {
-		String[] values = valuestrue[i];
+	/***
+	 * 
+	 * @param duckulaPackage
+	 * @param remove           需要删除id
+	 * @param valuestrue       值
+	 * @param filterColIndexOf 过滤列所在序号
+	 * @param pattern          过滤模式
+	 * @param filterValue      过滤条件的值
+	 * @param rowIndex         第几行
+	 */
+	private void filter(DuckulaPackage duckulaPackage, Map<Integer, Boolean> remove, String[][] valuestrue,
+			int filterColIndexOf, FilterPattern pattern, String filterValue, int rowIndex) {
+		String[] values = valuestrue[rowIndex];// 数据的值
 		switch (pattern) {
 		case regular:
-			boolean checkResult = StrPattern.checkStrFormat(value, values[indexOf]);
+			boolean checkResult = StrPattern.checkStrFormat(filterValue, values[filterColIndexOf]);
 			if (!checkResult) {
-				remove.put(i, true);
+				remove.put(rowIndex, true);
 			}
 			break;
 		case sql:
-			String[] colNameFormSql = getColNameFormSql(value);
-			String sql = value;
+			String[] colNameFormSql = getColNameFormSql(filterValue);
+			String sql = filterValue;
 			for (String tempCol : colNameFormSql) {
 				sql = sql.replace(String.format("${%s}", tempCol), "?");
 			}
@@ -248,7 +260,7 @@ public class BusiFilter implements IBusi {
 			for (int j = 0; j < colNameFormSql.length; j++) {
 				int indexOf2 = ArrayUtils.indexOf(duckulaPackage.getEventTable().getCols(), colNameFormSql[j]);
 				if (StringUtil.isNull(values[indexOf2])) {// 20190813 如果有值为空就直接过滤
-					remove.put(i, true);
+					remove.put(rowIndex, true);
 					return;
 				} else {
 					queryParams[j] = values[indexOf2];
@@ -262,10 +274,10 @@ public class BusiFilter implements IBusi {
 				JdbcAssit.setPreParam(prst, queryParams);
 				ResultSet rs = prst.executeQuery();
 				if (!rs.next()) {
-					remove.put(i, true);
+					remove.put(rowIndex, true);
 					// log.info("remo:{}", i);
 				} else {
-					log.info("need send:{},remove:{}", i, remove.size());
+					log.info("need send no:{},remove:{}", rowIndex, remove.size());
 				}
 				rs.close();
 			} catch (Exception e) {
